bb23f999a034aa3af0986335c3bc39a88e589304
ntua-el18845 <el18845@mail.ntua.gr>
2023-01-19 03:33:13 +0200

    Add files via upload

---
+++ api-backend/models/Answers.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const AnswerSchema = new mongoose.Schema({
+    questionnaireID: {
+        type: String,
+        required: true
+    },
+
+    session: {
+        type: String,
+        required: true
+    },
+
+    answers: [
+        {
+            qID: {
+                type:String,
+                required:true
+            },
+
+            ans: {
+                type:String
+            }
+        }
+    ]
+})
+
+module.exports = mongoose.model('Answers', AnswerSchema);
