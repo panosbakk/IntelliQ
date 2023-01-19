42a11bdba51ba29255b2f2005219b8939cab0e0c
ntua-el18845 <el18845@mail.ntua.gr>
2023-01-19 03:33:09 +0200

    Add files via upload

---
+++ api-backend/routes/getQuestionnaire.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const getQuestionnairebyid = require('../controllers/getQuestionnaire');
+
+router.get("/:questionnaireID", getQuestionnairebyid.getQuestionnairebyid);
+
+module.exports = router;
