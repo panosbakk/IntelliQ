05c9b5818d5b8e23b6252a84ca0de6041b0b7e0a
ntua-el18845 <el18845@mail.ntua.gr>
2023-01-19 03:33:15 +0200

    Frontend survey submission working!

--- frontend/src/components/SurveyComponent.js
+++ frontend/src/components/SurveyComponent.js
@@ -2,15 +2,55 @@ import React, { Component } from "react";
 import * as Survey from "survey-react";
 import "survey-react/survey.css";
 
+function generateRandomString() {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 4; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 class SurveyComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { isCompleted: false };
+    this.state = {
+      isCompleted: false,
+      surveyName: this.props.json.questionnaireID,
+    };
     this.onCompleteComponent = this.onCompleteComponent.bind(this);
   }
-  onCompleteComponent() {
+
+  onCompleteComponent = (result) => {
     this.setState({ isCompleted: true });
-  }
+    const data = result.data;
+    const questionnaireID = this.state.surveyName;
+    const session = generateRandomString();
+    console.log(session);
+    Object.keys(data).forEach(async (key) => {
+      let questionID = key;
+      let optionID = data[key];
+      console.log(questionID, optionID);
+      try {
+        const res = await fetch(
+          `http://localhost:9103/intelliq_api/doanswer/${questionnaireID}/${questionID}/${session}/${optionID}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ question: key, answer: data[key] }),
+          }
+        );
+        const jsonData = await res.json();
+        console.log(jsonData);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  };
 
   jsonModifier(json) {
     let surveyJson = {
@@ -57,7 +97,7 @@ class SurveyComponent extends Component {
     let surveyRender = !this.state.isCompleted ? (
       <Survey.Survey
         json={json}
-        showCompletedPage={false}
+        showCompletedPage={true}
         onComplete={this.onCompleteComponent}
       />
     ) : null;
