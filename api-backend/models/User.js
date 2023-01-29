25158b7931e6ffdc53671001b9392b36e5a94d00
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-01-29 20:30:52 +0200

    Update User.js

--- api-backend/models/User.js
+++ api-backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     username: {type: String, required:true, unique:true},
     password: {type: String, required:true},
     isAdmin: {type: Boolean, default:false},
