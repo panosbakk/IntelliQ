8390c9057d980fe48144b11c964b4b673cc954b9
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-01-31 18:09:48 +0200

    Updated App.js with two buttons, one for user register and one for user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import { Homepage } from "./components/Homepage";
@@ -6,29 +6,54 @@ import { SurveyPage } from "./components/SurveyPage";
 import { SurveyListPage } from "./components/SurveyListPage";
 import { StatisticsListPage } from "./components/StatisticsListPage";
 import { NotFound } from "./components/NotFound";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <NavBar />
 
-        <main className="container">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/survey/:id" element={<SurveyPage />} />
-            <Route path="/survey-list" element={<SurveyListPage />} />
-            <Route
-              path="/survey/statistics/:id"
-              element={<StatisticsListPage />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-      </>
-    );
-  }
-}
+const App = () => {
+  let history = useNavigate();
+
+  const handleRegisterClick = () => {
+    history.push("/register");
+  };
+
+  const handleLoginClick = () => {
+    history.push("/login");
+  };
+
+  return (
+    <>
+      <NavBar />
+      <div className="d-flex justify-content-end">
+        <Link to="/register">
+        <button className="btn btn-primary mr-3" onClick={handleRegisterClick}>
+          Register
+        </button>
+        </Link>
+        <Link to="/login">
+        <button className="btn btn-secondary" onClick={handleLoginClick}>
+          Login
+        </button>
+        </Link>
+      </div>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/survey/:id" element={<SurveyPage />} />
+          <Route path="/survey-list" element={<SurveyListPage />} />
+          <Route
+            path="/survey/statistics/:id"
+            element={<StatisticsListPage />}
+          />
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </>
+  );
+};
 
 export default App;
+
