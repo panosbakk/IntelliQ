b5f9d866d6725922998cb0438aa935e609f428ae
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-01-31 18:13:55 +0200

    Update server.js

--- api-backend/server.js
+++ api-backend/server.js
@@ -9,24 +9,25 @@ const getQuestionRoute = require("./routes/getQuestion");
 const getSessionAnswersRoute = require("./routes/getSessionAnswers");
 const getQuestionAnswersRoute = require("./routes/getQuestionAnswers");
 const postQuestionAnswerRoute = require("./routes/postQuestionAnswer");
+const registerRoutes = require("./routes/registerRoutes");
+const loginRoutes = require("./routes/loginRoutes");
 const {
   getQuestionnaireCount,
 } = require("./controllers/getQuestionnaireCount");
 
 const port = process.env.PORT || 9103;
-const username = process.env.MONGO_USERNAME;
-const pwd = process.env.MONGO_PASS;
 const app = express();
 app.use(express.json());
 app.use(cors());
 mongoose.set("strictQuery", false);
 mongoose.connect(
-  `mongodb+srv://${username}:${pwd}@intelliq.25mg5s4.mongodb.net/?retryWrites=true&w=majority`
+  process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 );
 
-app.get("/intelliq_api", (req, res) => {
-  res.send({ status: "API Working!" });
-});
+
 
 app.use("/intelliq_api/questionnaire", getQuestionnaireRoute);
 
@@ -39,6 +40,10 @@ app.use("/intelliq_api/getsessionanswers", getSessionAnswersRoute);
 app.use("/intelliq_api/getquestionanswers", getQuestionAnswersRoute);
 
 app.use("/intelliq_api/doanswer", postQuestionAnswerRoute);
+
+app.use("/intelliq_api", registerRoutes);
+
+app.use("/intelliq_api", loginRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
