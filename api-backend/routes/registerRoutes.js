8fa5f62c31939b310a04cd5a839ec7feadc25158
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-01-31 18:12:47 +0200

    added register routes

---
+++ api-backend/routes/registerRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password, admin } = req.body;
+    const user = new User({
+      username,
+      password,
+      admin,
+    });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+    const payload = {
+      user: {
+        id: user.id,
+        admin: user.admin,
+      },
+    };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;
