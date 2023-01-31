c615cf7dd31b9a0ed2fc527a1221a709e10d5a98
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-01-31 18:10:48 +0200

    Updated Login.js (bug)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,48 +1,41 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetch('http://localhost:9103/intelliq_api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
-        })
-        .then(res => res.json())
-        .then(data => {
-            localStorage.setItem('token', data.token);
-            window.location.href = '/';
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await axios.post('http://localhost:9103/intelliq_api/login', { username, password });
+      localStorage.setItem('token', response.data.token);
+      window.location.href = '/';
+    } catch (error) {
+      setError('Incorrect username or password');
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                placeholder="Username" 
-                value={username} 
-                onChange={e => setUsername(e.target.value)} 
-            />
-            <input 
-                type="password" 
-                placeholder="Password" 
-                value={password} 
-                onChange={e => setPassword(e.target.value)} 
-            />
-            <button type="submit">Login</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Login</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
 };
 
 export default Login;
