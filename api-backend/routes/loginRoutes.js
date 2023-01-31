57ed89e57c37d6139e595d71602dcbfe13c3c6ce
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-01-31 18:12:20 +0200

    updated login routes

--- api-backend/routes/loginRoutes.js
+++ api-backend/routes/loginRoutes.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
-const loginRoute = require('../controllers/loginController');
 
-router.post("/login", loginRoute.login);
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // Search for the user in the database
+    const user = await User.findOne({ username });
+
+    if (!user) return res.status(401).json({ error: 'Incorrect username or password' });
+
+    // Compare the password with the hashed password in the database
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
+    if (!isMatch) return res.status(401).json({ error: 'Incorrect username or password' });
+
+    // Create a JSON Web Token
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 360000 });
+
+    // Send the token as the response
+    res.send({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'An error occurred. Please try again later.' });
+  }
+});
 
 module.exports = router;
