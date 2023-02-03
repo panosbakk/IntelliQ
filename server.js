353b7e3d65af2d4add324d5e6274f0b2936b3894
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-02-04 00:36:34 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const app = require('./app')
+const mongoose = require('mongoose')
+const config = require('config')
+
+if(!config.has('jwtPrivateKey')){
+    console.log('FATAL ERROR: jwtPrivateKey is not defined.')
+    process.exit(1)
+}
+
+// Configuring the database
+const db = config.get('db')
+
+mongoose.Promise = global.Promise;
+mongoose.set('strictQuery', false);
+// Connecting to the database
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Successfully connected to the database.");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+
+// listen for requests
+app.listen(9103, () => {
+    console.log("Server is listening on port 9103.");
+});
