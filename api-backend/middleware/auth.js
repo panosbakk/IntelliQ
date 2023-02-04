212f766dba373448bdf3004abe562c87b1785749
ntua-el18845 <el18845@mail.ntua.gr>
2023-02-04 02:55:31 +0200

    Cli dependencies

--- api-backend/middleware/auth.js
+++ api-backend/middleware/auth.js
@@ -1,16 +1,15 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-function auth(req, res, next){
-    const token = req.header('X-OBSERVATORY-AUTH')
-    if(!token) return res.status(401).send('Access denied. No token provided.')
-    try{
-        const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
-        req.user = decoded
-        next()
-    }
-    catch{
-        res.status(400).send('Access denied. Invalid Token.')
-    }
+function auth(req, res, next) {
+  const token = req.header("X-OBSERVATORY-AUTH");
+  if (!token) return res.status(401).send("Access denied. No token provided.");
+  try {
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(400).send("Access denied. Invalid Token.");
+  }
 }
 exports.auth = auth;
