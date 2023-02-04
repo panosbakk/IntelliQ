3af6f4d89bdaf77e16baceb2b395131ef3bf7d1f
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-02-04 21:37:01 +0200

    updated adminroutes to protect /admin

--- api-backend/routes/adminRoutes.js
+++ api-backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     const { healthCheck, resetall } = require('../controllers/adminController')
-    
-
-    app.post('/intelliq_api/admin/resetall', resetall)
-    app.get('/intelliq_api/admin/healthcheck', healthCheck);
+    const { auth } = require('../middleware/auth');
+    const { admin } = require('../middleware/admin');
+    app.post('/intelliq_api/admin/resetall', [auth, admin], resetall)
+    app.get('/intelliq_api/admin/healthcheck', [auth, admin], healthCheck);
 }
