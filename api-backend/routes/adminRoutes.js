256b320c9cb5356ece5737e9ab64d41caee823ee
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-02-06 16:48:54 +0200

    added the updated routes to include resetquestionnaire

--- api-backend/routes/adminRoutes.js
+++ api-backend/routes/adminRoutes.js
@@ -1,7 +1,8 @@
 module.exports = (app) => {
-    const { healthCheck, resetall } = require('../controllers/adminController')
+    const { healthCheck, resetall, resetQuestionnaire } = require('../controllers/adminController')
     const { auth } = require('../middleware/auth');
     const { admin } = require('../middleware/admin');
     app.post('/intelliq_api/admin/resetall', [auth, admin], resetall)
     app.get('/intelliq_api/admin/healthcheck', [auth, admin], healthCheck);
+    app.post('/intelliq_api/admin/resetq/:questionnaireID', [auth, admin], resetQuestionnaire);
 }
