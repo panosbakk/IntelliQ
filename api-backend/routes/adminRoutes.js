b1e667d0786f4a5f9530762fa7a68be118b320eb
ntua-el19600 <115112839+ntua-el19600@users.noreply.github.com>
2023-02-08 21:14:32 +0200

    api done

--- api-backend/routes/adminRoutes.js
+++ api-backend/routes/adminRoutes.js
@@ -1,8 +1,12 @@
 module.exports = (app) => {
-    const { healthCheck, resetall, resetQuestionnaire } = require('../controllers/adminController')
+    const { healthCheck, resetall, resetQuestionnaire, uploadQuestionnaire } = require('../controllers/adminController')
     const { auth } = require('../middleware/auth');
     const { admin } = require('../middleware/admin');
+    const multer = require('multer');
+    const upload = require("multer")({ storage: multer.memoryStorage() });
+
     app.post('/intelliq_api/admin/resetall', [auth, admin], resetall)
     app.get('/intelliq_api/admin/healthcheck', [auth, admin], healthCheck);
+    app.post('/intelliq_api/admin/questionnaire_upd', [auth, admin, upload.single('file')], uploadQuestionnaire);
     app.post('/intelliq_api/admin/resetq/:questionnaireID', [auth, admin], resetQuestionnaire);
 }
