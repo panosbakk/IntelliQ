aeb30e0272e941108a45a6f9e0fba8d19ddcd928
ntua-el19600 <el19600@mail.ntua.gr>
2023-02-16 20:32:10 +0200

    s

--- api-backend/controllers/postQuestionAnswer.js
+++ api-backend/controllers/postQuestionAnswer.js
@@ -9,39 +9,46 @@ exports.postQuestionAnswer = async (req, res) => {
     const session = req.params.session;
     const optionID = req.params.optionID;
 
-    if (!questionnaireID || !questionID || !session || !optionID) {
+    if (!questionnaireID || !session || !optionID) {
       return badRequest(
         res,
-        "Invalid request. QuestionnaireID, QuestionID, Session and OptionID are all required parameters."
+        "Invalid request. QuestionnaireID, Session and OptionID are all required parameters."
       );
     }
-
+    
+    if (!questionID) {
+      return badRequest(res, "QuestionID is a required parameter.");
+    }
+    
     // Find or create an answer document for the given questionnaireID and session
     let answer = await Answers.findOneAndUpdate(
       { questionnaireID: questionnaireID, session: session },
       { questionnaireID: questionnaireID, session: session },
       { upsert: true, new: true }
     );
-
+    
+    // Find the answer in the answers array, and update or add the answer
     const answerIndex = answer.answers.findIndex((ans) => ans.qID === questionID && ans.ans === optionID);
-if (answerIndex >= 0) {
-  // The answer already exists, no updates needed
-  res.sendStatus(204);
-} else {
-  // Find the index of the answer in the answers array
-  const answerIndex = answer.answers.findIndex((ans) => ans.qID === questionID);
-  if (answerIndex >= 0) {
-    // Update the existing answer
-    answer.answers[answerIndex].ans = optionID;
-  } else if (questionID) { // Add a new answer only if questionID is truthy
-    // Add a new answer to the answers array
-    answer.answers.push({ qID: questionID, ans: optionID });
-  }
-  // Save the answer
-  await answer.save();
-  res.sendStatus(204);
-}
-
+    
+    if (answerIndex >= 0) {
+      // The answer already exists, no updates needed
+      res.sendStatus(204);
+    } else {
+      // Find the index of the answer in the answers array
+      const answerIndex = answer.answers.findIndex((ans) => ans.qID === questionID);
+      if (answerIndex >= 0) {
+        // Update the existing answer
+        answer.answers[answerIndex].ans = optionID;
+      } else {
+        // Add a new answer to the answers array
+        answer.answers.push({ qID: questionID, ans: optionID });
+      }
+    
+      // Save the answer
+      await answer.save();
+      res.sendStatus(204);
+    }
+    
 
 
   } catch (error) {
