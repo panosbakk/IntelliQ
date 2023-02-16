39f18a40a7d7a943cf84bd14d070ad7f29aec0f4
ntua-el19600 <el19600@mail.ntua.gr>
2023-02-16 20:53:44 +0200

    c

--- api-backend/controllers/postQuestionAnswer.js
+++ api-backend/controllers/postQuestionAnswer.js
@@ -20,7 +20,7 @@ exports.postQuestionAnswer = async (req, res) => {
     let answer = await Answers.findOneAndUpdate(
       { questionnaireID: questionnaireID, session: session },
       { questionnaireID: questionnaireID, session: session },
-      { upsert: true, new: true }
+      { upsert: true }
     );
 
     const answerIndex = answer.answers.findIndex((ans) => ans.qID === questionID && ans.ans === optionID);
