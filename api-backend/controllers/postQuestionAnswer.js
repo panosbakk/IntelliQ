102b32405c6d5408d1eaaaa9afd283491600f431
ntua-el18845 <el18845@mail.ntua.gr>
2023-02-16 20:09:22 +0200

    .

--- api-backend/controllers/postQuestionAnswer.js
+++ api-backend/controllers/postQuestionAnswer.js
@@ -24,27 +24,29 @@ exports.postQuestionAnswer = async (req, res) => {
     );
 
     // Find the answer in the answers array, and update or add the answer
-    const answerIndex = answer.answers.findIndex((ans) => ans.qID === questionID && ans.ans === optionID);
-if (answerIndex >= 0) {
-  // The answer already exists, no updates needed
-  res.sendStatus(204);
-} else {
-  // Find the index of the answer in the answers array
-  const answerIndex = answer.answers.findIndex((ans) => ans.qID === questionID);
-  if (answerIndex >= 0) {
-    // Update the existing answer
-    answer.answers[answerIndex].ans = optionID;
-  } else {
-    // Add a new answer to the answers array
-    answer.answers.push({ qID: questionID, ans: optionID });
-  }
+    const answerIndex = answer.answers.findIndex(
+      (ans) => ans.qID === questionID && ans.ans === optionID
+    );
+    if (answerIndex >= 0) {
+      // The answer already exists, no updates needed
+      res.sendStatus(204);
+    } else {
+      // Find the index of the answer in the answers array
+      const answerIndex = answer.answers.findIndex(
+        (ans) => ans.qID === questionID
+      );
+      if (answerIndex >= 0) {
+        // Update the existing answer
+        answer.answers[answerIndex].ans = optionID;
+      } else {
+        // Add a new answer to the answers array
+        answer.answers.push({ qID: questionID, ans: optionID });
+      }
 
-  // Save the answer
-  await answer.save();
-  res.sendStatus(204);
-}
-
-
+      // Save the answer
+      await answer.save();
+      res.sendStatus(204);
+    }
   } catch (error) {
     badRequest(res, error);
   }
