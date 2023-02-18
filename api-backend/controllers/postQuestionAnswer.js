3483aa52f7f3599bcc0f0cc1a4b0af34b00d4cc4
ntua-el19600 <el19600@mail.ntua.gr>
2023-02-18 17:22:11 +0200

    fixed post

--- api-backend/controllers/postQuestionAnswer.js
+++ api-backend/controllers/postQuestionAnswer.js
@@ -1,66 +1,74 @@
-const Answers = require("../models/Answers");
-const Questionnaire = require("../models/Questionnaire");
-const badRequest = require("../utils/badRequestResponse");
-const internalServerError = require("../utils/internalServerErrorResponse");
+const mongoose = require('mongoose');
+const Answers = require('../models/Answers');
+const Questionnaire = require('../models/Questionnaire');
+const badRequest = require('../utils/badRequestResponse');
+const internalServerError = require('../utils/internalServerErrorResponse');
 
 exports.postQuestionAnswer = async (req, res) => {
+  const session = mongoose.startSession();
   try {
     const questionnaireID = req.params.questionnaireID;
     const questionID = req.params.questionID;
-    const session = req.params.session;
     const optionID = req.params.optionID;
 
     if (!questionnaireID || !questionID || !session || !optionID) {
       return badRequest(
         res,
-        "Invalid request. QuestionnaireID, QuestionID, Session and OptionID are all required parameters."
+        'Invalid request. QuestionnaireID, QuestionID, Session and OptionID are all required parameters.'
       );
     }
 
-    // Check if questionnaireID, questionID and optionID exist in the Questionnaire collection
-    const questionnaire = await Questionnaire.findOne({
-      questionnaireID: questionnaireID,
-      "questions.qID": questionID,
-    });
+    const sessionOptions = { readPreference: 'primary', readConcern: { level: 'local' }, writeConcern: { w: 'majority' } };
+    const transactionResults = await session.withTransaction(async () => {
+      // Check if questionnaireID, questionID and optionID exist in the Questionnaire collection
+      const questionnaire = await Questionnaire.findOne({
+        questionnaireID: questionnaireID,
+        'questions.qID': questionID,
+      }).session(session);
 
-    if (!questionnaire) {
-      return badRequest(
-        res,
-        "Invalid request. QuestionnaireID, QuestionID, or OptionID do not exist in the database."
+      if (!questionnaire) {
+        throw new Error(
+          'Invalid request. QuestionnaireID, QuestionID, or OptionID do not exist in the database.'
+        );
+      }
+
+      // Find or create an answer document for the given questionnaireID and session
+      let answer = await Answers.findOneAndUpdate(
+        { questionnaireID: questionnaireID, session: session },
+        { questionnaireID: questionnaireID, session: session },
+        { upsert: true, new: true, session: session }
       );
-    }
 
-    // Find or create an answer document for the given questionnaireID and session
-    let answer = await Answers.findOneAndUpdate(
-      { questionnaireID: questionnaireID, session: session },
-      { questionnaireID: questionnaireID, session: session },
-      { upsert: true, new: true }
-    );
-
-    const answerIndex = answer.answers.findIndex(
-      (ans) => ans.qID === questionID && ans.ans === optionID
-    );
-    if (answerIndex >= 0) {
-      // The answer already exists, no updates needed
-      res.sendStatus(200);
-    } else {
-      // Find the index of the answer in the answers array
       const answerIndex = answer.answers.findIndex(
-        (ans) => ans.qID === questionID
+        (ans) => ans.qID === questionID && ans.ans === optionID
       );
       if (answerIndex >= 0) {
-        // Update the existing answer
-        answer.answers[answerIndex].ans = optionID;
-      } else if (questionID) {
-        // Add a new answer only if questionID is truthy
-        // Add a new answer to the answers array
-        answer.answers.push({ qID: questionID, ans: optionID });
+        // The answer already exists, no updates needed
+        return { status: 'OK' };
+      } else {
+        // Find the index of the answer in the answers array
+        const answerIndex = answer.answers.findIndex(
+          (ans) => ans.qID === questionID
+        );
+        if (answerIndex >= 0) {
+          // Update the existing answer
+          answer.answers[answerIndex].ans = optionID;
+        } else if (questionID) {
+          // Add a new answer only if questionID is truthy
+          // Add a new answer to the answers array
+          answer.answers.push({ qID: questionID, ans: optionID });
+        }
+        // Save the answer
+        await answer.save({ session: session });
+        return { status: 'OK' };
       }
-      // Save the answer
-      await answer.save();
-      res.sendStatus(200);
-    }
+    }, sessionOptions);
+
+    res.sendStatus(200);
   } catch (error) {
-    badRequest(res, error);
+    console.error(error);
+    internalServerError(res, error);
+  } finally {
+    await session.endSession();
   }
 };
